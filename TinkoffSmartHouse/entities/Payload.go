031525c6c60b3525501e5ad11945d6400ea5bc3a
Maxim Golchanskiy <golchanskiy.m.d@gmail.com>
2024-07-20 09:42:36 +0300

    Implement all util functions

--- TinkoffSmartHouse/entities/Payload.go
+++ TinkoffSmartHouse/entities/Payload.go
@@ -15,7 +15,75 @@ type Payload struct {
 }
 
 func parseCmdBody(device constants.DEV_TYPE, cmd constants.CMD, cmdBodyBytes []byte) ByteInterface {
-	return &Packet{}
+	switch {
+	case (device == constants.HUB || device == constants.SOCKET || device == constants.LAMP || device == constants.CLOCK) && (cmd == constants.WHOISHERE || cmd == constants.IAMHERE):
+		nameLen := cmdBodyBytes[0]
+		name := cmdBodyBytes[1 : nameLen+1]
+		return Name{string(name)}
+	case (device == constants.SENSOR) && (cmd == constants.WHOISHERE || cmd == constants.IAMHERE):
+		nameLength := cmdBodyBytes[0]
+		name := cmdBodyBytes[1 : nameLength+1]
+		sensors := cmdBodyBytes[nameLength+1]
+		triggerLength := cmdBodyBytes[nameLength+2]
+		triggers := make([]Trigger, triggerLength)
+		curSkip := int(nameLength) + 3
+		for i := 0; i < int(triggerLength); i++ {
+			curOp := cmdBodyBytes[curSkip]
+			curSkip++
+			curVal, skipULEB := util.DecodeULEB128(cmdBodyBytes[curSkip:])
+			curNameLen := cmdBodyBytes[curSkip+skipULEB]
+			curName := string(cmdBodyBytes[curSkip+skipULEB+1 : curSkip+skipULEB+1+int(curNameLen)])
+			curSkip += skipULEB + int(curNameLen) + 1
+			curTrigger := Trigger{
+				Op:    curOp,
+				Value: curVal,
+				Name:  curName,
+			}
+			triggers[i] = curTrigger
+		}
+		return Sensors{
+			DevName: string(name),
+			DevProps: EnvSensorsProperties{
+				Sensors:  sensors,
+				Triggers: triggers,
+			},
+		}
+	case cmd == constants.GETSTATUS && (device == constants.SOCKET || device == constants.LAMP || device == constants.SENSOR || device == constants.SWITCH):
+		return nil
+	case cmd == constants.STATUS && device == constants.SENSOR:
+		valuesSize := int(cmdBodyBytes[0])
+		values := make([]int, valuesSize)
+		curSkip := 1
+		for i := 0; i < valuesSize; i++ {
+			curVal, skipULEB := util.DecodeULEB128(cmdBodyBytes[curSkip:])
+			values[i] = curVal
+			curSkip += skipULEB
+		}
+		return Sensor{Values: values}
+	case device == constants.SWITCH && (cmd == constants.WHOISHERE || cmd == constants.IAMHERE):
+		nameLength := cmdBodyBytes[0]
+		name := cmdBodyBytes[1 : nameLength+1]
+		devNamesLen := int(cmdBodyBytes[nameLength+1])
+		devNames := make([]string, devNamesLen)
+		curSkip := nameLength + 2
+		for i := 0; i < devNamesLen; i++ {
+			curNameLen := cmdBodyBytes[curSkip]
+			curName := cmdBodyBytes[curSkip+1 : curSkip+1+curNameLen]
+			devNames[i] = string(curName)
+			curSkip += curNameLen + 1
+		}
+		return Switch{
+			DevName:  string(name),
+			DevProps: DevProps{DevNames: devNames},
+		}
+	case device == constants.CLOCK && cmd == constants.TICK:
+		timeTick, _ := util.DecodeULEB128(cmdBodyBytes)
+		return Timestamp{Timestamp: timeTick}
+	case ((cmd == constants.STATUS) && (device == constants.LAMP || device == constants.SOCKET || device == constants.SWITCH)) || ((cmd == constants.SETSTATUS) && (device == constants.LAMP || device == constants.SOCKET)):
+		v := cmdBodyBytes[0]
+		return Value{Value: v}
+	}
+	return nil
 }
 
 func (pl Payload) toBytes() []byte {
--- TinkoffSmartHouse/util/UtilFunctions.go
+++ TinkoffSmartHouse/util/UtilFunctions.go
@@ -3,19 +3,56 @@ package util
 import (
 	"TinkoffSmartHouse/constants"
 	"fmt"
+	"strings"
+	"unicode"
 )
 
-func EncodeULEB128(int) []byte {
-	return make([]byte, 0)
+func EncodeULEB128(v int) []byte {
+	var byteArr []byte
+	for {
+		byte_ := byte(v & 0x7F)
+		v >>= 7
+		if v != 0 {
+			v |= 0x80
+		}
+		byteArr = append(byteArr, byte_)
+		if v == 0 {
+			break
+		}
+	}
+	return byteArr
 }
 
 // ans/ bytes_parsed
-func DecodeULEB128([]byte) (int, int) {
-	return 0, 0
+func DecodeULEB128(bytes []byte) (int, int) {
+	res := 0
+	shift := 0
+	bytesParsed := 0
+	for _, bt := range bytes {
+		bytesParsed++
+		res |= (int(bt) & 0x7f) << shift
+		shift += 7
+		if bt&0x80 == 0 {
+			break
+		}
+	}
+	return res, bytesParsed
 }
 
-func CRC8Simple([]byte) byte {
-	return 0
+func CRC8Simple(bytes []byte) byte {
+	const mask byte = 0x1D
+	crc8 := byte(0)
+	for _, curByte := range bytes {
+		crc8 ^= curByte
+		for i := 0; i < 8; i++ {
+			if (crc8 & 0x80) != 0 {
+				crc8 = (crc8 << 1) ^ mask
+			} else {
+				crc8 <<= 1
+			}
+		}
+	}
+	return crc8
 }
 
 func getConnection(url string) string {
@@ -25,6 +62,13 @@ func getConnection(url string) string {
 	return url
 }
 
-func removeSpaces(string) string {
-	return ""
+func removeSpaces(str string) string {
+	b := strings.Builder{}
+	b.Grow(len(str))
+	for _, ch := range str {
+		if !unicode.IsSpace(ch) {
+			b.WriteRune(ch)
+		}
+	}
+	return b.String()
 }

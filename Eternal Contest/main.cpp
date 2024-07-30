545da073f47b2319d9660ac7e9484ba57927af2a
Maxim Golchanskiy <golchanskiy.m.d@gmail.com>
2024-07-30 08:50:54 +0300

    Add graph and dp tasks

--- Eternal Contest/main.cpp
+++ Eternal Contest/main.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 
+const int thousand = 1e3+1;
+const int hundred = 100;
+
 int task1(){
   int a,b,c,d; std::cin >> a >> b >> c >> d;
   return d > b ? (a+(d-b)*c) : a;
@@ -129,8 +132,46 @@ std::pair<long , long> task6(){
     return std::make_pair(-1,-1);
 }
 
-std::pair<int, int> task7(){
+bool isNotCycle(std::vector<int>& b, int n){
+  std::vector<bool> used(n+1, false);
+  int now = 1;
+  used[now] = true;
+  while(!used[b[now]]){
+    now = b[now];
+    used[now] = true;
+  }
+  for(int i = 1; i <= n; ++i)
+    if(!used[i]) return false;
+  return true;
+}
 
+std::pair<int, int> task7(){
+  int n; std::cin >> n;
+  std::vector<int> a(thousand), b(thousand);
+  for(int i = 1; i <= n; ++i){
+    std::cin >> a[i];
+  }
+  std::vector<int> cnt(n+1);
+  for(int i = 1; i <= n; ++i) {
+    cnt[a[i]]++;
+    if(cnt[a[i]] > 2) return std::make_pair(-1,-1);
+  }
+  int zero = 0, two = 0;
+  for(int i = 1; i <= n; ++i){
+    if(cnt[i] == 0) zero = i;
+    else if(cnt[i] == 2) two = i;
+  }
+  if(zero == 0) return std::make_pair(-1,-1);
+  for(int i = 1; i <= n; ++i){
+    if(a[i] == two){
+      for(int j = 1; j <= n; ++j) b[j] = a[j];
+      b[i] = zero;
+      if(isNotCycle(b,n)){
+        return std::make_pair(i, zero);
+      }
+    }
+  }
+  return std::make_pair(-1,-1);
 }
 
 std::pair<double, double> task8(){
@@ -138,15 +179,57 @@ std::pair<double, double> task8(){
 }
 
 int task9(){
+  int n;
+  std::cin >> n;
+  std::vector<int> a(n+1);
+  for (int i = 1; i <= n; ++i) {
+    std::cin >> a[i];
+  }
+  const int INF = 2e9;
+  std::vector<std::vector<int>> dp(n+1, std::vector<int>(n+1, INF));
 
+  dp[0][0] = 0;
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 0; j <= i; ++j) {
+      dp[i][j] = std::min(dp[i][j], dp[i-1][j] + a[i]);
+      if (a[i] > 100 && j + 1 <= i) {
+        dp[i][j+1] = std::min(dp[i][j+1], dp[i-1][j] + a[i]);
+      }
+      if (j >= 1) {
+        dp[i][j-1] = std::min(dp[i][j-1], dp[i-1][j]);
+      }
+    }
+  }
+
+  int ans = INF;
+  for (int i = 0; i <= n; ++i) {
+    ans = std::min(ans, dp[n][i]);
+  }
+  std::cout << ans << std::endl;
+  return ans;
 }
 
 double task10(){
 
 }
 
-int task11(){
+long long pow(long long a, long long n, long long p){
+  long long res = 1;
+  while(n > 0){
+    if(n%2) res = (res*a)%p;
+    a = (a*a)%p;
+    n /=2;
+  }
+  return res;
+}
 
+long long task11(){
+  long long l,r,p; std::cin >> l >> r >> p;
+  long long ans = 0;
+  for(long long i = l; i <= r; ++i){
+    ans = (ans + pow(i, p-2, p))%p;
+  }
+  return ans;
 }
 
 long long task12(){
@@ -155,6 +238,5 @@ long long task12(){
 
 int main()
 {
-  auto t = task6();
-  std::cout << t.first << " " << t.second;
+  std::cout << task11();
 }
